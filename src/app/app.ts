dc102331d3afb67317751b3083d0db8a41eb5e21
Armin <dzarma.busatlic@gmail.com>
2025-06-02 21:19:06 +0200

    data binding component to exercise ways to communicate between component ts and html file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { User } from './user/user';
+import { DataBinding } from './data-binding/data-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, User],
+  imports: [RouterOutlet, User, DataBinding],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/data-binding/data-binding.ts
+++ src/app/data-binding/data-binding.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
-  styleUrl: './data-binding.scss'
+  styleUrl: './data-binding.scss',
 })
 export class DataBinding {
-
+  name: string = 'Armin';
+  topic: string = 'Learning Angular 20';
+  image: string =
+    'https://cdn.bqwatches.com/images/cc78a997-1b33-4c97-93e3-bc4ff3b16b8d.jpg';
+  onSave() {
+    alert('Data Saved');
+  }
+  countries: string[] = ['Germany', 'France', 'Italy', 'Spain', 'Norway'];
+  selectedCountry: string = '';
+  onCountrySelect(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedCountry = selectElement.value;
+  }
+  random: any = '';
 }
