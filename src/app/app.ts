fd4841e9040251fd588bdb65088f3a0748cf922f
Armin <dzarma.busatlic@gmail.com>
2025-06-03 10:05:16 +0200

    adding bootstrap, structural directives old and new way to handle conditional rendering

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { User } from './user/user';
 import { DataBinding } from './data-binding/data-binding';
+import { Directives } from './directives/directives';
+import { StructuralDirectiveNgifVsIf } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, User, DataBinding],
+  imports: [
+    RouterOutlet,
+    User,
+    DataBinding,
+    Directives,
+    StructuralDirectiveNgifVsIf,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.ts
@@ -0,0 +1,24 @@
+import { CommonModule, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-structural-directive-ngif-vs-if',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './structural-directive-ngif-vs-if.html',
+  styleUrl: './structural-directive-ngif-vs-if.scss',
+})
+export class StructuralDirectiveNgifVsIf {
+  isChecked: boolean = false;
+  onClick() {
+    this.isChecked = !this.isChecked;
+  }
+  isInputBox: boolean = true;
+  showField() {
+    this.isInputBox = true;
+  }
+  hideField() {
+    this.isInputBox = false;
+  }
+  input1: string = '';
+  input2: string = '';
+}
