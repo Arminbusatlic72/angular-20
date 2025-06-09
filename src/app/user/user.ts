1e1ee1ef30021baab474d09b71735b6eef80c69a
Armin <dzarma.busatlic@gmail.com>
2025-06-09 14:52:33 +0200

    Life cycle hooks used to log the messages in user component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-user',
@@ -6,8 +18,54 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
-export class User {
-  constructor(private router: Router) {}
+export class User
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  topic: string;
+  constructor(private router: Router) {
+    this.topic = 'Angular 19';
+    console.log('Constructor of User component called');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    // use for component input property changes
+    console.log('1. ngOnChanges called with changes:', changes);
+  }
+  ngOnInit(): void {
+    // use this hook for API call
+    console.log('2. ngOnInit called');
+  }
+  ngDoCheck(): void {
+    // use this hook for custom change detection
+    console.log('3. ngDoCheck called');
+  }
+  ngAfterContentInit(): void {
+    // use this hook for content projection
+    console.log('4. ngAfterContentInit called');
+  }
+  ngAfterContentChecked(): void {
+    // use this hook for content projection
+    console.log('5. ngAfterContentChecked called');
+  }
+  ngAfterViewInit(): void {
+    // use this hook for view initialization
+    console.log('6. ngAfterViewInit called');
+  }
+  ngAfterViewChecked(): void {
+    // use this hook for view changes
+    console.log('7. ngAfterViewChecked called');
+  }
+  ngOnDestroy(): void {
+    // use this hook for cleanup
+    console.log('8. ngOnDestroy called');
+  }
   loginData() {
     // user verification logic completed
     // this.router.navigate(['/data-binding']);
